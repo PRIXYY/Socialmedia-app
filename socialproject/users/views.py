c848c2dcbc3dd868cf838a60e405f39507daeea3
Preet <preet16vaishnav@gmail.com>
2024-06-03 23:55:20 +0530

    Minor change

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -3,5 +3,7 @@ from .forms import LoginForm
 
 # Create your views here.
 def user_login(request):
+    if request.method =="POST":
+        
     form = LoginForm()
     return render(request,'users/login.html',{'form':form})
