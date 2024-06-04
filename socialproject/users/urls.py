6bdb3eb1f826ecec25c6ae2b67c13bcbb8c30e4b
Preet <preet16vaishnav@gmail.com>
2024-06-04 23:59:12 +0530

    Working on logout page

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/',views.user_login,name='login'),
+    path("logout/", auth_views.LogoutView.as_view( http_method_names=["post", "get", "options"] )),
+
 ]
