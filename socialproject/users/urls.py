107c285188284a223b25d79e5668446ee2c4fbf2
Preet <preet16vaishnav@gmail.com>
2024-06-05 23:54:14 +0530

    Change password functionality

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('login/',views.user_login,name='login'),
     path("logout/", auth_views.LogoutView.as_view(http_method_names=["post", "get", "options"],template_name='users/logout.html'), name='logout'),
+    path('password_change/',auth_views.PasswordChangeView.as_view(template_name='users/password_change_form.html'),name='password_change'),
+    path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(template_name='users/password_cdone.html'),name='password_change_done'),
 ]
