dc38d8daab2d9a28cb0f417ba7232d0b166d4402
Preet <preet16vaishnav@gmail.com>
2024-06-05 23:31:40 +0530

    Created base template and home page for users

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
+    path('',views.index,name='index'),
     path('login/',views.user_login,name='login'),
-    path("logout/", auth_views.LogoutView.as_view( http_method_names=["post", "get", "options"] )),
-
+    path("logout/", auth_views.LogoutView.as_view(http_method_names=["post", "get", "options"],template_name='users/logout.html'), name='logout'),
 ]
